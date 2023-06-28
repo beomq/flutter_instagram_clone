4ea943142c62e7a3f5bf8c9c510226354a2119e1
beomq <beomdolq@gmail.com>
2023-06-28 11:48:57 +0900

    MODIFY: 가로모드시 공사중 표시

--- lib/tab/home/home_page.dart
+++ lib/tab/home/home_page.dart
@@ -24,99 +24,101 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              'instagram 에 오신걸 환영 합니다',
-              style: TextStyle(fontSize: 26),
-            ),
-            const Text(
-              '사진과 동영상을 보려면 팔로우하세요',
-            ),
-            Card(
-              elevation: 4.0,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  children: [
-                    SizedBox(
-                      width: 80,
-                      height: 80,
-                      child: GestureDetector(
-                        onTap: () async {
-                          await model.updateProfileImage();
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                'instagram 에 오신걸 환영 합니다',
+                style: TextStyle(fontSize: 26),
+              ),
+              const Text(
+                '사진과 동영상을 보려면 팔로우하세요',
+              ),
+              Card(
+                elevation: 4.0,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        width: 80,
+                        height: 80,
+                        child: GestureDetector(
+                          onTap: () async {
+                            await model.updateProfileImage();
 
-                          setState(() {});
-                        },
-                        child: CircleAvatar(
-                          backgroundImage:
-                              NetworkImage(model.getProfileImageUrl()),
+                            setState(() {});
+                          },
+                          child: CircleAvatar(
+                            backgroundImage:
+                                NetworkImage(model.getProfileImageUrl()),
+                          ),
                         ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    Text(
-                      model.getEmail(),
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      model.getNickname(),
-                    ),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    Row(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.network(
-                          'https://cdn.pixabay.com/photo/2018/04/23/14/23/giraffe-3344366_1280.jpg',
-                          width: 70,
-                          height: 70,
-                          fit: BoxFit.cover,
-                        ),
-                        const SizedBox(
-                          width: 5,
-                        ),
-                        Image.network(
-                          'https://cdn.pixabay.com/photo/2013/07/19/00/18/giraffe-165200_1280.jpg',
-                          width: 70,
-                          height: 70,
-                          fit: BoxFit.cover,
-                        ),
-                        const SizedBox(
-                          width: 5,
-                        ),
-                        Image.network(
-                          'https://cdn.pixabay.com/photo/2019/09/17/20/47/prague-4484517_1280.jpg',
-                          width: 70,
-                          height: 70,
-                          fit: BoxFit.cover,
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    const Text('Facebook 친구'),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('팔로우'),
-                    ),
-                  ],
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        model.getEmail(),
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        model.getNickname(),
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.network(
+                            'https://cdn.pixabay.com/photo/2018/04/23/14/23/giraffe-3344366_1280.jpg',
+                            width: 70,
+                            height: 70,
+                            fit: BoxFit.cover,
+                          ),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          Image.network(
+                            'https://cdn.pixabay.com/photo/2013/07/19/00/18/giraffe-165200_1280.jpg',
+                            width: 70,
+                            height: 70,
+                            fit: BoxFit.cover,
+                          ),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          Image.network(
+                            'https://cdn.pixabay.com/photo/2019/09/17/20/47/prague-4484517_1280.jpg',
+                            width: 70,
+                            height: 70,
+                            fit: BoxFit.cover,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      const Text('Facebook 친구'),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('팔로우'),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
