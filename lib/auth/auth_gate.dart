457e784b9fbc953ab232b47ca2276f6906a92f43
beomq <beomdolq@gmail.com>
2023-08-01 21:22:42 +0900

    fix: ios, android 각각 client id 사용 하게함

--- lib/auth/auth_gate.dart
+++ lib/auth/auth_gate.dart
@@ -3,13 +3,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutterfire_ui/auth.dart';
 import 'package:instagram_clone/tab/tab_page.dart';
+import 'dart:io' show Platform;
 
 class AuthGate extends StatelessWidget {
   const AuthGate({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final String? clientId = dotenv.env['CLIENT_ID'];
+    final String? clientId;
+    if (Platform.isIOS) {
+      clientId = dotenv.env['APPLE_CLIENT_ID'];
+    } else if (Platform.isAndroid) {
+      clientId = dotenv.env['ANDROID_CLIENT_ID'];
+    } else {
+      clientId = '';
+    }
 
     return StreamBuilder<User?>(
       stream: FirebaseAuth.instance.authStateChanges(),
