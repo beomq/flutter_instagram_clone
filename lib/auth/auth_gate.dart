c7ce7438c23d00f2b11bb1cc03c3b6f5069d739d
beomq <beomdolq@gmail.com>
2023-08-01 20:56:08 +0900

    feat: 구글 로그인 추가

--- lib/auth/auth_gate.dart
+++ lib/auth/auth_gate.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutterfire_ui/auth.dart';
 import 'package:instagram_clone/tab/tab_page.dart';
 
@@ -8,13 +9,18 @@ class AuthGate extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final String? clientId = dotenv.env['CLIENT_ID'];
+
     return StreamBuilder<User?>(
       stream: FirebaseAuth.instance.authStateChanges(),
       builder: (context, snapshot) {
         if (!snapshot.hasData) {
           return SignInScreen(
-            providerConfigs: const [
-              EmailProviderConfiguration(),
+            providerConfigs: [
+              const EmailProviderConfiguration(),
+              GoogleProviderConfiguration(
+                clientId: '$clientId',
+              ),
             ],
             headerBuilder: (context, constraints, _) {
               return const Center(
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:instagram_clone/auth/auth_gate.dart';
 import 'package:instagram_clone/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
